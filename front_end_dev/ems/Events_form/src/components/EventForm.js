32eb23d28af8bbf4f89796e03dda3b3e62a195c0
Mysha-rah <rajitarah79@gmail.com>
2025-02-06 14:45:00 -0600

    Update EventForm.js

--- front_end_dev/ems/Events_form/src/components/EventForm.js
+++ front_end_dev/ems/Events_form/src/components/EventForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../index.css';
+import '../event.css';
 import { BrowserRouter as Router } from 'react-router-dom';  // Ensure Router is imported and wrapped around your app
 import AdminProfile from './AdminProfile';
 import EventManagement from './EventManagement';
@@ -9,7 +9,7 @@ const EventForm = () => {
     <Router> {/* Wrap your components inside Router */}
       <div className="app-container">
         <AdminProfile/>
-        <EventManagement />
+        <EventManagement/>
       </div>
     </Router>
   );
