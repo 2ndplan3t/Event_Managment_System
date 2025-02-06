ec5fd7406519a3257a159e4526300e1fd875fffe
Mysha-rah <rajitarah79@gmail.com>
2025-02-06 14:47:04 -0600

    Update index.js

--- front_end_dev/ems/Events_form/src/index.js
+++ front_end_dev/ems/Events_form/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './event.css';
 import EventForm  from './components/EventForm';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <EventForm  />
+    <EventForm/>
   </React.StrictMode>
 );
 
