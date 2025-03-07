24985750085af0297474a263be2cd2f4219834ba
m-nguyen01 <myann.nguyen01@gmail.com>
2025-03-07 12:05:57 -0600

    commit before merge

--- front_end_dev/ems/src/pages/Profile/User/Navigation.jsx
+++ front_end_dev/ems/src/pages/Profile/User/Navigation.jsx
@@ -1,32 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
-        const [user, setUser] = useState(null);
-        const navigate = useNavigate();
-      
-        useEffect(() => {
-          const storedUser = localStorage.getItem("user");
-          if (storedUser) {
-            setUser(JSON.parse(storedUser));
-          }
-        }, []);
-      
-        const handleLogout = async () => {
-          try {
-            await fetch("http://localhost:5000/api/logout", {
-              method: "POST",
-              credentials: "include",
-            });
-            localStorage.removeItem("user");
-            localStorage.removeItem("adminId");
-            setUser(null);
-            navigate("/");
-          } catch (error) {
-            console.error("Logout failed:", error);
-          }
-        };
     return (
         <nav className="navbar">
             <ul>
@@ -34,7 +10,6 @@ function Navbar() {
                 <li><Link to="/user">Profile</Link></li>
                 <li><Link to="/notifications">Notifications</Link></li>
                 <li><Link to="/history">History</Link></li>
-                <li><Link to="" onClick={handleLogout}>Logout</Link></li>
             </ul>
         </nav>
     );
