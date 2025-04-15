6b0dcfbfff8c5ac7c984236b67cd2931ef71e7b1
My-Ann <105829868+m-nguyen01@users.noreply.github.com>
2025-04-15 14:30:52 -0500

    Update Navigation to have logout.jsx

--- front_end_dev/ems/src/pages/Profile/User/Navigation.jsx
+++ front_end_dev/ems/src/pages/Profile/User/Navigation.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const handleLogout = async () => {
+    try {
+      await fetch("http://localhost:5000/api/logout", {
+        method: "POST",
+        credentials: "include", 
+      });
+      localStorage.removeItem("user");
+      localStorage.removeItem("adminId"); 
+      setUser(null);
+      navigate("/");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
     return (
         <nav className="navbar">
             <ul>
@@ -10,6 +26,7 @@ function Navbar() {
                 <li><Link to="/user">Profile</Link></li>
                 <li><Link to="/notifications">Notifications</Link></li>
                 <li><Link to="/history">History</Link></li>
+                <li><span className="logout-text" onClick={handleLogout}>Logout</span></li>
             </ul>
         </nav>
     );
