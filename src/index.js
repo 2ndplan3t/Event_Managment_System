5199cd77624da0fea698acfe4d65aaf8a01dcc9d
Mysha-rah <rajitarah79@gmail.com>
2025-04-19 00:22:45 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AdminReports from './AdminReports.jsx';
-import './AdminReports.css'; // optional, only if you have it
+import './AdminReports.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
